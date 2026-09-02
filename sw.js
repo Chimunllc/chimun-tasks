e1db3e08ee5c9939f57c8ee2ba3cc84e4f8f32b4
Munkh-Uchral Ganbat <ceo@nomaadcamp.com>
2026-09-02 18:41:39 +0800

    sw: bump v763

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_VERSION whenever index.html or assets change so phones pick up new code.
  */
 
-const CACHE_VERSION = 'chimun-tasks-v762-nomaad-cash-month-2026-09-02';
+const CACHE_VERSION = 'chimun-tasks-v763-ceo-income-unify-2026-09-02';
 const SHELL_FILES = [
   './',
   './index.html',

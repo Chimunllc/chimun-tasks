dd53c1ef60c55707d6e563f0e93d517525e72a64
Munkh-Uchral Ganbat <ceo@nomaadcamp.com>
2026-09-03 12:32:18 +0800

    sw: bump v788

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_VERSION whenever index.html or assets change so phones pick up new code.
  */
 
-const CACHE_VERSION = 'chimun-tasks-v787-perm-labels-2026-09-02';
+const CACHE_VERSION = 'chimun-tasks-v788-order-search-email-2026-09-02';
 const SHELL_FILES = [
   './',
   './index.html',
